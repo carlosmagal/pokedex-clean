d112beb553b8d1e507a58f590bf190b18a56764a
Carlos Magalhaes <carlosmsilva755@gmail.com>
2022-03-07 15:57:19 -0300

    firebase cloud messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:pokedex_clean/app_module.dart';
 import 'package:pokedex_clean/app_screen.dart';
+import 'package:pokedex_clean/features/presenter/firebase_messaging/custom_firebase_messaging.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  await CustomFirebaseMessaging().inicialize();
+  await CustomFirebaseMessaging().getFirebaseToken();
+  
   runApp(
     ModularApp(
       module: AppModule(),
